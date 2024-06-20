daa6933f4fb2b21a554fcef8172d2f4afc46abee
mxplayermovies <dsouzarnd@gmail.com>
2024-06-20 19:35:25 +0530

    TRACKER

--- pages/_app.js
+++ pages/_app.js
@@ -42,7 +42,7 @@ function Application({ Component, pageProps }) {
 
       
  <Script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=664f7ed93a56e900196c14e4&product=sticky-share-buttons&source=platform" async="async"></Script>
-        <GoogleAnalytics measurementId="G-S5LDTN7VFD" />
+        <GoogleAnalytics measurementId="G-9R90C84JLQ" />
         {/* <Script async data-id="101405628" src="//static.getclicky.com/js"></Script> */}
      
         <PageTransition>  
