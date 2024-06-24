f9574d9180324672956694d1386dcca9fde3ad1b
mxplayermovies <dsouzarnd@gmail.com>
2024-06-24 09:59:06 +0530

    Clean up go

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://youtubemagazine.vercel.app/',
+    siteUrl: 'https://moviesmagazine.onrender.com/',
     changefreq: 'daily',
     priority: 0.90,
     sitemapSize: 7000,
