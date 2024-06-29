f526683f4e30aa3d59e362cf3991f6f8d41a14bd
mxplayermovies <dsouzarnd@gmail.com>
2024-06-29 08:05:49 +0530

    yes fuck again

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://moviesmagazine.vercel.app/',
+    siteUrl: 'https://123moviemagazine.vercel.app/',
     changefreq: 'daily',
     priority: 0.90,
     sitemapSize: 7000,
