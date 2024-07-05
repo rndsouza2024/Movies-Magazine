016bd79838079114dbd5a15fad48d9713151d4a4
mxplayermovies <dsouzarnd@gmail.com>
2024-07-05 19:40:38 +0530

    YES UPDATED NOW

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://123moviesmagazine.vercel.app/',
+    changefreq: 'daily',
+    priority: 0.90,
+    sitemapSize: 7000,
+  }
