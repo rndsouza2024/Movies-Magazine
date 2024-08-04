213881bc8d0034119ac37add334b787a3e9d49b8
mxplayermovies <dsouzarnd@gmail.com>
2024-08-05 04:39:01 +0530

    yes

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://123moviesmagazine.vercel.app/',
-    changefreq: 'daily',
-    priority: 0.90,
-    sitemapSize: 7000,
-  }
